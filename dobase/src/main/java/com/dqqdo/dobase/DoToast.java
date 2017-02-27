c938a4c0e8033ae367fb504e028b6b4a0fa14cb4
hasaki <932539350@qq.com>
2017-02-27 17:42:48 +0800

    解决疯狂点击饼图时toast久久不消失的问题。

--- dobase/src/main/java/com/dqqdo/dobase/DoToast.java
+++ dobase/src/main/java/com/dqqdo/dobase/DoToast.java
@@ -14,6 +14,11 @@ import android.widget.Toast;
  *Toast 封装类。
  */
 public class DoToast {
+
+	private static String oldMsg;
+	protected static Toast toast = null;
+	private static long oneTime = 0;
+	private static long twoTime = 0;
 	
 	public static void shortToast(Context context, int resId) {
 		showToast(context, resId, Toast.LENGTH_SHORT);
@@ -48,7 +53,7 @@ public class DoToast {
 		showToast(context, text, duration);
 	}
 	
-	public static void showToast(Context context, String text, int duration) {
+	public static void showToast(Context context, String s, int duration) {
 		if (context == null){
 			return;
 		}
@@ -57,8 +62,25 @@ public class DoToast {
 	            return;
 	        }
 		}
-		if(!TextUtils.isEmpty(text) && !"".equals(text.trim())){
-			Toast.makeText(context, text, duration).show();
+		if(!TextUtils.isEmpty(s) && !"".equals(s.trim())){
+			if (toast == null) {
+				toast = Toast.makeText(context, s, Toast.LENGTH_SHORT);
+				toast.show();
+				oneTime = System.currentTimeMillis();
+			} else {
+				if (s == null) s="null";
+				twoTime = System.currentTimeMillis();
+				if (s.equals(oldMsg)) {
+					if (twoTime - oneTime > Toast.LENGTH_SHORT) {
+						toast.show();
+					}
+				} else {
+					oldMsg = s;
+					toast.setText(s);
+					toast.show();
+				}
+			}
+			oneTime = twoTime;
 		}
 	}
 }
