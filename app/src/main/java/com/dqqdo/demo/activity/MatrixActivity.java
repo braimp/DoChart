e9191944b7e9163e6a45c3a8fd99ab31881a08a4
zmobs <zmobs0@163.com>
2017-09-15 15:25:50 +0800

    继续完善 矩阵测试部分代码

--- app/src/main/java/com/dqqdo/demo/activity/MatrixActivity.java
+++ app/src/main/java/com/dqqdo/demo/activity/MatrixActivity.java
@@ -1,7 +1,24 @@
 package com.dqqdo.demo.activity;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import com.dqqdo.demo.R;
 import com.dqqdo.dobase.BaseActivity;
+import com.dqqdo.dochart.util.LogUtil;
 
 /**
  * 作者：duqingquan
@@ -10,23 +27,25 @@ import com.dqqdo.dobase.BaseActivity;
 public class MatrixActivity extends BaseActivity {
 
 
+
     @Override
     protected void initView() {
-
     }
 
 
     @Override
     protected void initData() {
 
-
-
     }
+
 
     @Override
     protected int getContentView() {
         return R.layout.activity_matrix;
     }
+
+
+
 
 
 }
--- dochart/src/main/java/com/dqqdo/dochart/ui/view/MatrixView.java
+++ dochart/src/main/java/com/dqqdo/dochart/ui/view/MatrixView.java
@@ -5,14 +5,19 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.dqqdo.dochart.R;
+import com.dqqdo.dochart.util.LogUtil;
 
 
 /**
@@ -26,19 +31,45 @@ public class MatrixView extends View{
     Matrix matrix = new Matrix();
     Paint mPaint = new Paint();
     Bitmap mBitmap;
+    RectF srcRectF,destRectF;
+
 
     /**
      * 组件初始化方法
      */
     private void init(Context context) {
         mBitmap = BitmapFactory.decodeResource(context.getResources(),R.drawable.foo);
-        matrix.postTranslate(100,100);
+        matrix.preTranslate(100,100);
         matrix.preScale(0.5f,0.5f);
+        matrix.setSinCos(1,1);
+//        matrix.preRotate(15,0,0);
+//        matrix.postRotate(45);
+//        matrix.preSkew(0,0.5f);
 
+        // 判断matrix矩阵性质
+//        LogUtil.d("matrix  isIdentity  ------ " + matrix.isIdentity());
+//        if(Build.VERSION.SDK_INT >= 21){
+//            LogUtil.d("matrix  isAffine  ------ " + matrix.isAffine());
+//        }
+
+        // rect stay rect
+        srcRectF = new RectF(20,20,120,120);
+        destRectF = new RectF(20,20,120,120);
+
+        mPaint.setColor(Color.RED);
+        boolean rectStaysRect = matrix.rectStaysRect();
+        LogUtil.d("rectStaysRect -----  " + rectStaysRect);
+//        boolean result = matrix.setRectToRect(srcRectF,destRectF, Matrix.ScaleToFit.START);
+//        LogUtil.d("result -----  " + result);
+
+
+        // poly to poly
+//        float[] src = {0, 0};
+//        int DX = 300;
+//        float[] dst = {0 + DX, 0 + DX};
+//        matrix.setPolyToPoly(src, 0, dst, 0, 1);
 
     }
-
-
 
 
     public MatrixView(Context context) {
@@ -66,6 +97,10 @@ public class MatrixView extends View{
     @Override
     public void onDraw(Canvas canvas) {
         canvas.drawBitmap(mBitmap,matrix,mPaint);
+//        mPaint.setColor(Color.GREEN);
+//        canvas.drawRect(srcRectF,mPaint);
+//        mPaint.setColor(Color.BLUE);
+//        canvas.drawRect(destRectF,mPaint);
     }
 
 
