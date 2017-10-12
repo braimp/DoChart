abefb7f1da70a383fa2c02e2e67cf857d9946b6a
zmobs <zmobs0@163.com>
2017-10-12 10:35:23 +0800

    添加指标解析器 起步类。接下来准备完成自定义指标解释器

---
+++ dochart/src/main/java/com/dqqdo/dochart/resolver/IndexResolver.java
@@ -0,0 +1,51 @@
+package com.dqqdo.dochart.resolver;
+
+/**
+ * 指标解析器 接口定义
+ * 作者：duqingquan
+ * 时间：2017/10/10 17:43
+ */
+public interface IndexResolver {
+
+    /**
+     * 设置公式指标(json串)，返回错误码(返回0表示没有错误)
+     * @param strFormula
+     * @return
+     */
+    int setFormula(String strFormula);
+
+    /**
+     * 设置公式运行参数(json串)，返回错误码(返回0表示没有错误)
+     * @param strParam
+     * @return
+     */
+    int setParams(String strParam);
+
+    /**
+     * 设置数据(json串)，返回错误码(返回0表示没有错误)
+     * @param strData
+     * @return
+     */
+    int setData(String strData);
+
+    /**
+     * 执行公式，json串一次性返回所有结果集，如果有错误err_code会返回错误码。
+     * @return
+     */
+    String execute();
+
+    /**
+     * 执行公式，json串只返回结果集数量result_num，如果有错误err_code会返回错误码。
+     * @return
+     */
+    String executeEx();
+
+    /**
+     * 获取执行结果，json串返回指定的结果集，参数nStart表示起始位置，nCount表示取多少条结果。
+     * @param nStart
+     * @param nCount
+     * @return
+     */
+    String getResult(int nStart, int nCount);
+
+}
---
+++ dochart/src/main/java/com/dqqdo/dochart/resolver/StockIndexResolverImpl.java
@@ -0,0 +1,40 @@
+package com.dqqdo.dochart.resolver;
+
+/**
+ * 股票的指标解析器实现类
+ * 作者：duqingquan
+ * 时间：2017/10/10 17:44
+ */
+public class StockIndexResolverImpl implements IndexResolver{
+
+    @Override
+    public int setFormula(String strFormula) {
+        return 0;
+    }
+
+    @Override
+    public int setParams(String strParam) {
+        return 0;
+    }
+
+    @Override
+    public int setData(String strData) {
+        return 0;
+    }
+
+    @Override
+    public String execute() {
+        return null;
+    }
+
+    @Override
+    public String executeEx() {
+        return null;
+    }
+
+    @Override
+    public String getResult(int nStart, int nCount) {
+        return null;
+    }
+
+}
